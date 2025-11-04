0bf5b54860f60137e83720287f514a7a9d40a6cf
Cedric <cedigasser@gmail.com>
2025-11-04 14:50:39 +0100

    Initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/cedricgasser/to_do_for_notion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cedricgasser.to_do_for_notion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
